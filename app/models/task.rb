9411e5c4dad86e09f121cc891bffc1bc5afee3d1
Andrew Mo <andrewmo@berkeley.edu>
2022-03-29 14:56:17 -0700

    start associations

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,10 @@
 class Task < ApplicationRecord
+
+    belongs_to :user
+
+    validates_presence_of :name
+    validates_presence_of :category
+    validates_presence_of :priority
+    validates_presence_of :user
+    
 end
