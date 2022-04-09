7a7c8c1908b8402c5d8446e9c0506b6b9067a606
Simon Jovanovic <simonjov@berkeley.edu>
2022-04-09 04:10:50 -0700

    tetsing for loop

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -29,7 +29,7 @@ class TasksController < ApplicationController
     task.user_complete = user.name
     task.complete_time = DateTime.now
     task.save!
-    redirect_to tasks_path(category: params[:category])
+    redirect_to tasks_path
   end
 
   def new; end
